6764834ba1035f9d9398837e8af6996c49f86483
ilyapc <ilya.komendantov@list.ru>
2023-06-19 19:17:33 +0300

    broadcast receiver

--- app/src/main/java/com/example/studying/MainActivity.kt
+++ app/src/main/java/com/example/studying/MainActivity.kt
@@ -1,7 +1,11 @@
 package com.example.studying
 
+import android.content.Intent
+import android.content.IntentFilter
 import android.os.Bundle
+import android.util.Log
 import androidx.appcompat.app.AppCompatActivity
+import androidx.localbroadcastmanager.content.LocalBroadcastManager
 import com.example.studying.databinding.ActivityMainBinding
 
 class MainActivity : AppCompatActivity() {
@@ -9,8 +13,31 @@ class MainActivity : AppCompatActivity() {
         ActivityMainBinding.inflate(layoutInflater)
     }
 
+    private val localReceiver = LocalBroadcastReceiver()
+    private val systemReceiver = SystemBroadcastReceiver()
+
     override fun onCreate(savedInstanceState: Bundle?) {
         super.onCreate(savedInstanceState)
         setContentView(binding.root)
+
+
+        binding.textView.setOnClickListener {
+            val intent = Intent("com.example.studying")
+            LocalBroadcastManager.getInstance(this).sendBroadcast(intent)
+        }
+    }
+    override fun onResume() {
+        super.onResume()
+        // system
+        registerReceiver(systemReceiver, IntentFilter("android.intent.action.AIRPLANE_MODE"))
+        // local
+        LocalBroadcastManager.getInstance(this)
+            .registerReceiver(localReceiver, IntentFilter("com.example.studying"))
+    }
+
+    override fun onPause() {
+        super.onPause()
+        LocalBroadcastManager.getInstance(this).unregisterReceiver(localReceiver)
+        unregisterReceiver(systemReceiver)
     }
 }
---
+++ app/src/main/java/com/example/studying/SystemBroadcastReceiver.kt
@@ -0,0 +1,12 @@
+package com.example.studying
+
+import android.content.BroadcastReceiver
+import android.content.Context
+import android.content.Intent
+import android.util.Log
+
+class SystemBroadcastReceiver : BroadcastReceiver() {
+    override fun onReceive(p0: Context?, p1: Intent?) {
+        Log.d("attadag", "system")
+    }
+}
