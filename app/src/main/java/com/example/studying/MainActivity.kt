a787b2db0f57344a188c763f5d8d3dc19d536246
ilyapc <ilya.komendantov@list.ru>
2023-06-25 16:54:25 +0300

    get all audio

--- app/src/main/java/com/example/studying/MainActivity.kt
+++ app/src/main/java/com/example/studying/MainActivity.kt
@@ -1,16 +1,69 @@
 package com.example.studying
 
+import android.Manifest
+import android.content.pm.PackageManager
+import android.net.Uri
+import android.os.Build
 import android.os.Bundle
+import android.provider.MediaStore
+import androidx.activity.result.contract.ActivityResultContracts
 import androidx.appcompat.app.AppCompatActivity
+import androidx.core.content.ContextCompat
 import com.example.studying.databinding.ActivityMainBinding
+import com.google.android.material.snackbar.Snackbar
 
 class MainActivity : AppCompatActivity() {
     private val binding: ActivityMainBinding by lazy {
         ActivityMainBinding.inflate(layoutInflater)
     }
 
+    private val musics = mutableListOf<Uri>()
+
+    private val permissionReadExternalStorage =
+        registerForActivityResult(ActivityResultContracts.RequestPermission()) { result ->
+            if (result) {
+                getAllAudio()
+            } else {
+                if (Build.VERSION.SDK_INT >= Build.VERSION_CODES.M) {
+                    if (shouldShowRequestPermissionRationale(Manifest.permission.READ_EXTERNAL_STORAGE)) {
+                        Snackbar.make(
+                            binding.root,
+                            "i need this permission",
+                            Snackbar.LENGTH_LONG
+                        ).show()
+                    }
+                }
+            }
+        }
+
+    private fun getAllAudio() {
+        contentResolver.query(
+            MediaStore.Audio.Media.EXTERNAL_CONTENT_URI,
+            arrayOf(MediaStore.Audio.AudioColumns.DATA),
+            MediaStore.Audio.Media.IS_MUSIC,
+            null,
+            null
+        )?.use { cursor ->
+            while (cursor.moveToNext()) {
+                val data = cursor.getColumnIndex(MediaStore.Audio.Media.DATA)
+                //Log.d("attadag", "attadag ${cursor.getString(titleColIndex)}}")
+                musics.add(Uri.parse(cursor.getString(data)))
+            }
+        }
+    }
+
     override fun onCreate(savedInstanceState: Bundle?) {
         super.onCreate(savedInstanceState)
         setContentView(binding.root)
+        if (ContextCompat.checkSelfPermission(
+                this,
+                Manifest.permission.READ_EXTERNAL_STORAGE
+            ) == PackageManager.PERMISSION_GRANTED
+        ) {
+            getAllAudio()
+        }
+        else {
+            permissionReadExternalStorage.launch(Manifest.permission.READ_EXTERNAL_STORAGE)
+        }
     }
 }
