06d1389da2325add53258da82917b2f92187d18a
ilyapc <ilya.komendantov@list.ru>
2023-06-28 09:42:45 +0300

    screen-flip

--- app/src/main/java/com/example/studying/MainActivity.kt
+++ app/src/main/java/com/example/studying/MainActivity.kt
@@ -1,6 +1,7 @@
 package com.example.studying
 
 import android.os.Bundle
+import android.util.Log
 import androidx.appcompat.app.AppCompatActivity
 import com.example.studying.databinding.ActivityMainBinding
 
@@ -12,5 +13,41 @@ class MainActivity : AppCompatActivity() {
     override fun onCreate(savedInstanceState: Bundle?) {
         super.onCreate(savedInstanceState)
         setContentView(binding.root)
+        Log.d("attadag", "onCreate")
+    }
+
+    override fun onStart() {
+        super.onStart()
+        Log.d("attadag", "onStart")
+    }
+
+    override fun onResume() {
+        super.onResume()
+        Log.d("attadag", "onResume")
+    }
+
+    override fun onPause() {
+        super.onPause()
+        Log.d("attadag", "onPause")
+    }
+
+    override fun onStop() {
+        super.onStop()
+        Log.d("attadag", "onStop")
+    }
+
+    override fun onDestroy() {
+        super.onDestroy()
+        Log.d("attadag", "onDestroy")
+    }
+
+    override fun onSaveInstanceState(outState: Bundle) {
+        super.onSaveInstanceState(outState)
+        Log.d("attadag", "onSaveInstanceState")
+    }
+
+    override fun onRestoreInstanceState(savedInstanceState: Bundle) {
+        super.onRestoreInstanceState(savedInstanceState)
+        Log.d("attadag", "onRestoreInstanceState")
     }
 }
