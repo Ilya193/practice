21d479ec3f5fcaeb36713c42da252f00d5f4707d
ilyapc <ilya.komendantov@list.ru>
2023-09-13 11:07:15 +0300

    decoro

--- app/src/main/java/com/example/studying/MainActivity.kt
+++ app/src/main/java/com/example/studying/MainActivity.kt
@@ -1,8 +1,13 @@
 package com.example.studying
 
 import android.os.Bundle
+import android.telephony.PhoneNumberFormattingTextWatcher
 import androidx.appcompat.app.AppCompatActivity
 import com.example.studying.databinding.ActivityMainBinding
+import ru.tinkoff.decoro.MaskImpl
+import ru.tinkoff.decoro.slots.PredefinedSlots
+import ru.tinkoff.decoro.watchers.MaskFormatWatcher
+
 
 class MainActivity : AppCompatActivity() {
     private val binding: ActivityMainBinding by lazy {
@@ -12,5 +17,17 @@ class MainActivity : AppCompatActivity() {
     override fun onCreate(savedInstanceState: Bundle?) {
         super.onCreate(savedInstanceState)
         setContentView(binding.root)
+
+        val mask = MaskFormatWatcher(
+            MaskImpl.createTerminated(PredefinedSlots.RUS_PHONE_NUMBER)
+        )
+
+        mask.installOn(binding.etPhone)
+
+        binding.etPhone.addTextChangedListener(object : PhoneNumberFormattingTextWatcher() {
+            override fun onTextChanged(s: CharSequence?, start: Int, before: Int, count: Int) {
+
+            }
+        })
     }
 }
