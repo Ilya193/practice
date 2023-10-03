57273c46a0f8edf15560fd3c4ef79d73e18515ce
ilyapc <ilya.komendantov@list.ru>
2023-10-03 12:42:29 +0300

    change payload with library

--- app/src/main/java/com/example/studying/presentation/MainActivity.kt
+++ app/src/main/java/com/example/studying/presentation/MainActivity.kt
@@ -1,12 +1,17 @@
 package com.example.studying.presentation
 
-import android.content.Intent
 import android.os.Bundle
 import android.view.View
 import android.view.WindowManager
+import android.view.animation.AnimationUtils
 import androidx.appcompat.app.AppCompatActivity
+import com.elveum.elementadapter.SimpleBindingAdapter
+import com.elveum.elementadapter.adapter
+import com.elveum.elementadapter.addBinding
+import com.elveum.elementadapter.simpleAdapter
+import com.example.studying.R
 import com.example.studying.databinding.ActivityMainBinding
-import com.google.android.material.snackbar.Snackbar
+import com.example.studying.databinding.PostLayoutBinding
 import org.koin.androidx.viewmodel.ext.android.viewModel
 
 
@@ -16,9 +21,48 @@ class MainActivity : AppCompatActivity() {
     }
 
     private val mainViewModel: MainViewModel by viewModel()
-    private val adapter = PostsAdapter { index ->
+    /*private val adapter = PostsAdapter{ index ->
         mainViewModel.setFavorite(index)
-    //Snackbar.make(binding.root, title, Snackbar.LENGTH_SHORT).show()
+        //Snackbar.make(binding.root, title, Snackbar.LENGTH_SHORT).show()
+    }*/
+
+    private val adapter: SimpleBindingAdapter<PostUi.Success> by lazy {
+        simpleAdapter<PostUi.Success, PostLayoutBinding> {
+            areItemsSame = { old, new ->
+                old.same(new)
+            }
+            areContentsSame = { old, new ->
+                old.sameContent(new)
+            }
+            changePayload = { old, new ->
+                old.changePayload(new)
+            }
+
+            bindWithPayloads { item, payloads ->
+
+                if (payloads.isEmpty()) {
+                    tvTitle.text = item.title
+                    root.startAnimation(
+                        AnimationUtils.loadAnimation(
+                            root.context,
+                            R.anim.main_anim
+                        )
+                    )
+                }
+                else if (payloads[0] == true) {
+                    if (item.isFavorite)
+                        icFavorite.setBackgroundResource(R.drawable.baseline_favorite_24)
+                    else
+                        icFavorite.setBackgroundResource(R.drawable.baseline_favorite_border_24)
+                }
+            }
+
+            listeners {
+                root.onClick {
+                    mainViewModel.setFavorite(index())
+                }
+            }
+        }
     }
 
     override fun onCreate(savedInstanceState: Bundle?) {
@@ -28,6 +72,7 @@ class MainActivity : AppCompatActivity() {
         window.setSoftInputMode(
             WindowManager.LayoutParams.SOFT_INPUT_ADJUST_PAN
         );
+
 
         binding.rvPosts.adapter = adapter
         binding.rvPosts.setHasFixedSize(true)
