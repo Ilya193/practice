0df32329c794f578c4ebd28b6c13f8f8f40f6980
ilyapc <ilya.komendantov@list.ru>
2023-11-12 17:39:10 +0300

    ViewModelFactory

--- app/src/main/java/com/example/studying/MainActivity.kt
+++ app/src/main/java/com/example/studying/MainActivity.kt
@@ -2,6 +2,7 @@ package com.example.studying
 
 import android.os.Bundle
 import androidx.appcompat.app.AppCompatActivity
+import androidx.lifecycle.ViewModelProvider
 import com.example.studying.databinding.ActivityMainBinding
 
 class MainActivity : AppCompatActivity() {
@@ -12,5 +13,8 @@ class MainActivity : AppCompatActivity() {
     override fun onCreate(savedInstanceState: Bundle?) {
         super.onCreate(savedInstanceState)
         setContentView(binding.root)
+
+        val viewModel = ViewModelProvider(this, MainViewModelFactory())[MainViewModel::class.java]
+        viewModel.test()
     }
 }
---
+++ app/src/main/java/com/example/studying/MainViewModel.kt
@@ -0,0 +1,15 @@
+package com.example.studying
+
+import androidx.lifecycle.ViewModel
+
+class MainViewModel : ViewModel() {
+
+    init {
+        println("attadag init MainViewModel")
+    }
+
+    fun test() {
+        println("attadag test")
+    }
+}
+
