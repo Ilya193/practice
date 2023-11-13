b586753e7b24db7901403ab7c25fbcbeace9d8fb
ilyapc <ilya.komendantov@list.ru>
2023-11-13 15:03:31 +0300

    refactoring

--- app/src/main/java/com/example/studying/MainActivity.kt
+++ app/src/main/java/com/example/studying/MainActivity.kt
@@ -3,58 +3,79 @@ package com.example.studying
 import android.Manifest
 import android.app.NotificationChannel
 import android.app.NotificationManager
+import android.app.PendingIntent
 import android.content.Context
+import android.content.Intent
 import android.content.pm.PackageManager
+import android.net.Uri
 import android.os.Build
 import android.os.Bundle
-import android.util.Log
-import android.widget.Toast
+import android.os.VibrationEffect
+import android.os.Vibrator
+import android.provider.Settings
+import android.view.View
 import androidx.activity.result.contract.ActivityResultContracts
 import androidx.appcompat.app.AppCompatActivity
 import androidx.core.app.ActivityCompat
 import androidx.core.app.NotificationCompat
 import com.example.studying.databinding.ActivityMainBinding
-import com.google.android.material.snackbar.Snackbar
 
 class MainActivity : AppCompatActivity() {
     private val binding: ActivityMainBinding by lazy {
         ActivityMainBinding.inflate(layoutInflater)
     }
 
-    private val channelId = "mainChannelId"
-
     private val permission =
         registerForActivityResult(ActivityResultContracts.RequestPermission()) { result ->
             if (result) {
-                createNotification()
-            }
-            else {
+                createNotificationAndNotify()
+                binding.openSettingButton.visibility = View.GONE
+                binding.textView.text = "Hello world"
+            } else {
                 if (Build.VERSION.SDK_INT >= Build.VERSION_CODES.M) {
-                    if (shouldShowRequestPermissionRationale(Manifest.permission.POST_NOTIFICATIONS)) {
-                        Snackbar.make(binding.root, "i need this permission", Snackbar.LENGTH_LONG).show()
+                    if (!shouldShowRequestPermissionRationale(Manifest.permission.POST_NOTIFICATIONS)) {
+                        binding.openSettingButton.visibility = View.VISIBLE
                     }
                 }
             }
         }
 
-
     override fun onCreate(savedInstanceState: Bundle?) {
         super.onCreate(savedInstanceState)
         setContentView(binding.root)
 
+        binding.openSettingButton.setOnClickListener {
+            startActivity(Intent(Settings.ACTION_APPLICATION_DETAILS_SETTINGS).apply {
+                data = Uri.parse("package:$packageName")
+            })
+        }
+
         binding.textView.setOnClickListener {
-            if (ActivityCompat.checkSelfPermission(
-                    this,
-                    Manifest.permission.POST_NOTIFICATIONS
-                ) == PackageManager.PERMISSION_GRANTED
-            ) {
-                createNotification()
-            } else {
-                if (Build.VERSION.SDK_INT >= Build.VERSION_CODES.TIRAMISU) {
-                    permission.launch(Manifest.permission.POST_NOTIFICATIONS)
-                }
+            if (checkPermission()) createNotificationAndNotify()
+            else if (Build.VERSION.SDK_INT >= Build.VERSION_CODES.TIRAMISU) {
+                permission.launch(Manifest.permission.POST_NOTIFICATIONS)
+                binding.textView.text = getString(R.string.need_permission)
             }
         }
+    }
+
+    override fun onResume() {
+        super.onResume()
+        if (checkPermission()) {
+            binding.openSettingButton.visibility = View.GONE
+            binding.textView.text = "Hello world"
+        }
+        else if (Build.VERSION.SDK_INT >= Build.VERSION_CODES.TIRAMISU) {
+            permission.launch(Manifest.permission.POST_NOTIFICATIONS)
+            binding.textView.text = getString(R.string.need_permission)
+        }
+    }
+
+    private fun checkPermission(): Boolean {
+        return ActivityCompat.checkSelfPermission(
+                this,
+                Manifest.permission.POST_NOTIFICATIONS
+            ) == PackageManager.PERMISSION_GRANTED
     }
 
     private fun createNotificationChannel(): NotificationManager {
@@ -63,8 +84,8 @@ class MainActivity : AppCompatActivity() {
 
         if (Build.VERSION.SDK_INT >= Build.VERSION_CODES.O) {
             val channel = NotificationChannel(
-                channelId,
-                "mainChannel",
+                CHANNEL_ID,
+                CHANNEL_NAME,
                 NotificationManager.IMPORTANCE_HIGH
             )
             notificationManager.createNotificationChannel(channel)
@@ -73,14 +94,40 @@ class MainActivity : AppCompatActivity() {
         return notificationManager
     }
 
-    private fun createNotification() {
-        val notification = NotificationCompat.Builder(this, channelId)
+    private fun createNotificationAndNotify() {
+        val notification = NotificationCompat.Builder(this, CHANNEL_ID)
             .setSmallIcon(R.drawable.ic_launcher_background)
             .setContentTitle("Test title")
             .setContentText("Test text")
             .setPriority(NotificationCompat.PRIORITY_HIGH)
+            .setContentIntent(
+                PendingIntent.getActivity(
+                    this,
+                    1,
+                    Intent(this, MainActivity::class.java).apply {
+                        addFlags(Intent.FLAG_ACTIVITY_CLEAR_TASK)
+                    },
+                    PendingIntent.FLAG_IMMUTABLE
+                )
+            )
 
         val manager = createNotificationChannel()
         manager.notify(1, notification.build())
+        val vibrator = getSystemService(Context.VIBRATOR_SERVICE) as Vibrator
+        if (Build.VERSION.SDK_INT >= 26) {
+            vibrator.vibrate(
+                VibrationEffect.createOneShot(
+                    100,
+                    VibrationEffect.DEFAULT_AMPLITUDE
+                )
+            )
+        } else {
+            vibrator.vibrate(100)
+        }
+    }
+
+    companion object {
+        private const val CHANNEL_ID = "CHANNEL_ID"
+        private const val CHANNEL_NAME = "CHANNEL_NAME"
     }
 }
