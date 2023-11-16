5dff4cbbfb4a74b39843efac5b7c83ecfd25e4de
ilyapc <ilya.komendantov@list.ru>
2023-11-16 16:07:55 +0300

    implicit intent

--- app/src/main/java/com/example/studying/MainActivity.kt
+++ app/src/main/java/com/example/studying/MainActivity.kt
@@ -1,5 +1,6 @@
 package com.example.studying
 
+import android.content.Intent
 import android.os.Bundle
 import androidx.appcompat.app.AppCompatActivity
 import com.example.studying.databinding.ActivityMainBinding
@@ -12,5 +13,14 @@ class MainActivity : AppCompatActivity() {
     override fun onCreate(savedInstanceState: Bundle?) {
         super.onCreate(savedInstanceState)
         setContentView(binding.root)
+
+        binding.root.setOnClickListener {
+            val intent = Intent(Intent.ACTION_SEND)
+            intent.type = "text/plain"
+            intent.putExtra(Intent.EXTRA_EMAIL, arrayOf("dalvik@example.com"))
+            intent.putExtra(Intent.EXTRA_SUBJECT, "EXTRA_SUBJECT")
+            intent.putExtra(Intent.EXTRA_TEXT, "EXTRA_TEXT")
+            startActivity(Intent.createChooser(intent, "Send Email"))
+        }
     }
 }
