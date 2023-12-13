6fb4d89547c1deeda8147dcbef169adbfb2c7be9
ilyapc <ilya.komendantov@list.ru>
2023-12-13 15:23:10 +0300

    get applications

--- app/src/main/java/com/example/studying/MainActivity.kt
+++ app/src/main/java/com/example/studying/MainActivity.kt
@@ -1,5 +1,8 @@
 package com.example.studying
 
+import android.content.Intent
+import android.content.pm.ResolveInfo
+import android.net.Uri
 import android.os.Bundle
 import androidx.appcompat.app.AppCompatActivity
 import com.example.studying.databinding.ActivityMainBinding
@@ -12,5 +15,15 @@ class MainActivity : AppCompatActivity() {
     override fun onCreate(savedInstanceState: Bundle?) {
         super.onCreate(savedInstanceState)
         setContentView(binding.root)
+
+        val mainIntent = Intent(Intent.ACTION_MAIN)
+        mainIntent.addCategory(Intent.CATEGORY_LAUNCHER)
+        val apps = packageManager.queryIntentActivities(mainIntent, 0)
+        for (app in apps) { }
+
+        binding.root.setOnClickListener {
+            val intent = packageManager.getLaunchIntentForPackage("")
+            startActivity(intent)
+        }
     }
 }
