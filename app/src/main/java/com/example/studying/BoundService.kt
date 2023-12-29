0332b2e87a62cf2be312bb471c2173edb0f67332
ilyapc <ilya.komendantov@list.ru>
2023-12-29 17:18:45 +0300

    bound service

--- app/src/main/java/com/example/studying/BoundService.kt
+++ app/src/main/java/com/example/studying/BoundService.kt
@@ -4,31 +4,45 @@ import android.app.Service
 import android.content.Intent
 import android.os.Binder
 import android.os.IBinder
-import java.util.concurrent.Executors
 
 class BoundService : Service() {
 
-    private var block: ((Int) -> Unit)? = null
+    private var blockStart: (() -> Unit)? = null
+    private var blockStop: (() -> Unit)? = null
     override fun onBind(intent: Intent?): IBinder? = LocalBinder()
 
     override fun onUnbind(intent: Intent?): Boolean {
-        this@BoundService.block =  null
+        this@BoundService.blockStart = null
+        this@BoundService.blockStop = null
         return false
     }
 
     inner class LocalBinder : Binder() {
-        fun subscribe(block: (Int) -> Unit) {
-            this@BoundService.block = block
-            Executors.newSingleThreadExecutor().execute {
-                for (i in 0..100) {
-                    Thread.sleep(250)
-                    this@BoundService.block?.invoke(i)
-                }
+        fun subscribe(block: () -> Unit) {
+            this@BoundService.blockStart = block
+        }
+
+        fun unsubscribe(block: () -> Unit) {
+            this@BoundService.blockStop = block
+        }
+    }
+
+    override fun onStartCommand(intent: Intent?, flags: Int, startId: Int): Int {
+        when (intent?.getStringExtra(ACTION)) {
+            START_SERVICE -> blockStart?.invoke()
+            STOP_SERVICE -> {
+                blockStop?.invoke()
+                this@BoundService.blockStart = null
+                this@BoundService.blockStop = null
             }
         }
 
-        fun unsubscribe() {
-            this@BoundService.block =  null
-        }
+        return START_NOT_STICKY
+    }
+
+    companion object {
+        const val ACTION = "ACTION"
+        const val START_SERVICE = "START_SERVICE"
+        const val STOP_SERVICE = "STOP_SERVICE"
     }
 }
--- app/src/main/java/com/example/studying/MainActivity.kt
+++ app/src/main/java/com/example/studying/MainActivity.kt
@@ -6,8 +6,11 @@ import android.content.Intent
 import android.content.ServiceConnection
 import android.os.Bundle
 import android.os.IBinder
+import android.view.View
+import android.view.animation.TranslateAnimation
 import androidx.appcompat.app.AppCompatActivity
 import com.example.studying.databinding.ActivityMainBinding
+
 
 class MainActivity : AppCompatActivity() {
     private val binding: ActivityMainBinding by lazy {
@@ -20,19 +23,59 @@ class MainActivity : AppCompatActivity() {
         override fun onServiceConnected(name: ComponentName?, service: IBinder?) {
             binder = service as BoundService.LocalBinder
             binder?.subscribe {
-                println("attadag $it")
+                if (binding.container.visibility == View.INVISIBLE) {
+                    setVisibility(View.INVISIBLE)
+                }
+            }
+            binder?.unsubscribe {
+                if (binding.container.visibility == View.VISIBLE) {
+                    setVisibility(View.VISIBLE)
+                }
             }
         }
 
         override fun onServiceDisconnected(name: ComponentName?) {
-            binder?.unsubscribe()
+            binder?.unsubscribe {
+                if (binding.container.visibility == View.VISIBLE) {
+                    setVisibility(View.VISIBLE)
+                }
+            }
             binder = null
         }
+    }
+
+    private fun setVisibility(visibility: Int) {
+        val animate = if (visibility == View.INVISIBLE) {
+            binding.container.visibility = View.VISIBLE
+            TranslateAnimation(0f, 0f, binding.container.height.toFloat(), 0f)
+        }
+        else {
+            binding.container.visibility = View.INVISIBLE
+            TranslateAnimation(0f, 0f, 0f, binding.container.height.toFloat())
+        }
+        animate.duration = 500
+        animate.fillAfter = true
+        binding.container.startAnimation(animate)
     }
 
     override fun onCreate(savedInstanceState: Bundle?) {
         super.onCreate(savedInstanceState)
         setContentView(binding.root)
+
+        binding.start.setOnClickListener {
+            Intent(this, BoundService::class.java).also { intent ->
+                bindService(intent, connection, Context.BIND_AUTO_CREATE)
+                intent.putExtra(BoundService.ACTION, BoundService.START_SERVICE)
+                startService(intent)
+            }
+        }
+        binding.stop.setOnClickListener {
+            Intent(this, BoundService::class.java).also { intent ->
+                unbindService(connection)
+                intent.putExtra(BoundService.ACTION, BoundService.STOP_SERVICE)
+                startService(intent)
+            }
+        }
     }
 
     override fun onResume() {
