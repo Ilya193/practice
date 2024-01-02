47623aa96f6b7632c56c48331107c2d14826631c
ilyapc <ilya.komendantov@list.ru>
2024-01-02 17:59:45 +0300

    get file from uri

--- app/src/main/java/com/example/studying/MainActivity.kt
+++ app/src/main/java/com/example/studying/MainActivity.kt
@@ -1,16 +1,50 @@
 package com.example.studying
 
+import android.content.Intent
+import android.net.Uri
 import android.os.Bundle
+import androidx.activity.result.contract.ActivityResultContracts
 import androidx.appcompat.app.AppCompatActivity
 import com.example.studying.databinding.ActivityMainBinding
+import java.io.File
+import java.io.FileOutputStream
+import java.util.UUID
 
 class MainActivity : AppCompatActivity() {
     private val binding: ActivityMainBinding by lazy {
         ActivityMainBinding.inflate(layoutInflater)
     }
 
+    private val takePicture = this.registerForActivityResult(
+        ActivityResultContracts.StartActivityForResult()
+    ) {
+        it.data?.data?.let { fileUri ->
+            val file = getFileFromUri(fileUri)
+            //upload to server
+            //remove file from cache
+        }
+    }
+
     override fun onCreate(savedInstanceState: Bundle?) {
         super.onCreate(savedInstanceState)
         setContentView(binding.root)
+
+        binding.root.setOnClickListener {
+            val intent = Intent(Intent.ACTION_GET_CONTENT)
+            intent.type = "image/*"
+            takePicture.launch(intent)
+        }
+    }
+
+    private fun getFileFromUri(uri: Uri): File {
+        val inputStream = contentResolver.openInputStream(uri)
+        val file = File(cacheDir, "${UUID.randomUUID()}.jpg")
+        val outputStream = FileOutputStream(file)
+        inputStream?.use { input ->
+            outputStream.use { output ->
+                input.copyTo(output)
+            }
+        }
+        return file
     }
 }
