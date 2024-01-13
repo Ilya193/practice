cc4ab1fa56b5bcfeaad1198642bf2aa46b5dbca4
ilyapc <ilya.komendantov@list.ru>
2024-01-13 17:09:19 +0300

    ClipboardManager

--- app/src/main/java/com/example/studying/MainActivity.kt
+++ app/src/main/java/com/example/studying/MainActivity.kt
@@ -1,5 +1,8 @@
 package com.example.studying
 
+import android.content.ClipData
+import android.content.ClipboardManager
+import android.content.Context
 import android.os.Bundle
 import androidx.appcompat.app.AppCompatActivity
 import com.example.studying.databinding.ActivityMainBinding
@@ -12,5 +15,12 @@ class MainActivity : AppCompatActivity() {
     override fun onCreate(savedInstanceState: Bundle?) {
         super.onCreate(savedInstanceState)
         setContentView(binding.root)
+
+        val clipboardManager = getSystemService(Context.CLIPBOARD_SERVICE) as ClipboardManager
+
+        binding.root.setOnClickListener {
+            val text = binding.textView.text.toString()
+            clipboardManager.setPrimaryClip(ClipData.newPlainText("text", text))
+        }
     }
 }
