849887689035b3da04fa1b393d65838bbbca19d2
ilya21 <Ilya.komendantov@list.ru>
2024-03-14 12:00:51 +0300

    buildSrc

---
+++ buildSrc/src/main/java/Libs.kt
@@ -0,0 +1,13 @@
+object Libs {
+    const val ktorClientAndroid = "io.ktor:ktor-client-android:${Versions.ktor}"
+    const val ktorClientSerialization = "io.ktor:ktor-client-serialization:${Versions.ktor}"
+    const val ktorClientContentNegotiation = "io.ktor:ktor-client-content-negotiation:${Versions.ktor}"
+    const val ktorSerializationKotlinxJson = "io.ktor:ktor-serialization-kotlinx-json:${Versions.ktor}"
+
+    const val koin = "io.insert-koin:koin-android:${Versions.koin}"
+}
+
+object Versions {
+    const val ktor = "2.3.6"
+    const val koin = "3.3.2"
+}
