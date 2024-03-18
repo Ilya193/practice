202bd3cd2b2db1750f10619235585643fd0880a9
ilya21 <Ilya.komendantov@list.ru>
2024-03-18 22:38:49 +0300

    added notification

--- app/src/main/java/com/example/studying/MainActivity.kt
+++ app/src/main/java/com/example/studying/MainActivity.kt
@@ -4,10 +4,14 @@ import android.content.BroadcastReceiver
 import android.content.Context
 import android.content.Intent
 import android.content.IntentFilter
+import android.content.pm.PackageManager
+import android.os.Build
 import android.os.Bundle
 import android.util.Log
+import androidx.activity.result.contract.ActivityResultContracts
 import androidx.appcompat.app.AppCompatActivity
 import androidx.constraintlayout.widget.ConstraintLayout
+import androidx.core.content.ContextCompat
 import androidx.localbroadcastmanager.content.LocalBroadcastManager
 import com.example.studying.databinding.ActivityMainBinding
 import com.google.android.material.snackbar.Snackbar
@@ -21,19 +25,24 @@ class MainActivity : AppCompatActivity() {
 
     private val mainViewModel: MainViewModel by viewModel()
 
+    private val permission =
+        registerForActivityResult(ActivityResultContracts.RequestPermission()) { result ->
+        }
+
     override fun onCreate(savedInstanceState: Bundle?) {
         super.onCreate(savedInstanceState)
         setContentView(binding.root)
 
         FirebaseMessaging.getInstance().subscribeToTopic("topic")
 
-        //mainViewModel.fetchMessages()
-        /*        findViewById<TextView>(R.id.tvHello).setOnClickListener {
-                    mainViewModel.fetchMessages()
-                }*/
-
         findViewById<ConstraintLayout>(R.id.root).setOnClickListener {
             mainViewModel.createMessageWithFirestore("message")
+        }
+
+
+        if (Build.VERSION.SDK_INT >= Build.VERSION_CODES.TIRAMISU) {
+            if (!checkPermission())
+                permission.launch(android.Manifest.permission.POST_NOTIFICATIONS)
         }
     }
 
@@ -54,5 +63,12 @@ class MainActivity : AppCompatActivity() {
     override fun onPause() {
         super.onPause()
         LocalBroadcastManager.getInstance(this).unregisterReceiver(broadcastReceiver)
+    }
+
+    private fun checkPermission(): Boolean {
+        return ContextCompat.checkSelfPermission(
+            this,
+            android.Manifest.permission.POST_NOTIFICATIONS
+        ) == PackageManager.PERMISSION_GRANTED
     }
 }
--- app/src/main/java/com/example/studying/MessagingService.kt
+++ app/src/main/java/com/example/studying/MessagingService.kt
@@ -1,7 +1,15 @@
 package com.example.studying
 
+import android.app.NotificationChannel
+import android.app.NotificationManager
+import android.app.PendingIntent
+import android.content.Context
 import android.content.Intent
+import android.content.pm.PackageManager
+import android.os.Build
 import android.util.Log
+import androidx.core.app.NotificationCompat
+import androidx.core.content.ContextCompat
 import androidx.localbroadcastmanager.content.LocalBroadcastManager
 import com.google.firebase.messaging.FirebaseMessagingService
 import com.google.firebase.messaging.RemoteMessage
@@ -22,5 +30,25 @@ class MessagingService : FirebaseMessagingService() {
             putExtra("TITLE", title)
             putExtra("BODY", body)
         })
+
+        val notificationManager =
+            getSystemService(Context.NOTIFICATION_SERVICE) as NotificationManager
+
+        if (Build.VERSION.SDK_INT >= Build.VERSION_CODES.O) {
+            val channel = NotificationChannel(
+                "CHANNEL_ID",
+                "CHANNEL_NAME",
+                NotificationManager.IMPORTANCE_LOW
+            )
+            notificationManager.createNotificationChannel(channel)
+        }
+
+        val notification = NotificationCompat.Builder(this, "CHANNEL_ID")
+            .setSmallIcon(R.drawable.ic_launcher_background)
+            .setContentTitle(title)
+            .setContentText(body)
+            .setPriority(NotificationCompat.PRIORITY_LOW)
+
+        notificationManager.notify(1, notification.build())
     }
 }
